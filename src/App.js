e6d4d3cb4bfe7c8a2a46a956e07baa955ad4a36f
ZAKHAROV-artem <artemz092006@gmail.com>
2022-08-11 23:36:56 +0200

    finished main todo functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("todos"));
+    if (items) {
+      setTodos(items);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const deleteTodo = (delete_id) => {
+    todos = setTodos(todos.filter((todo) => todo.id !== delete_id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App sm:flex block">
+      <AddTodoForm todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 }
