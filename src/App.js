77493df9ea981b556949856f897006c63e3a4994
ZAKHAROV-artem <artemz092006@gmail.com>
2022-08-12 19:49:07 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
-import { useEffect, useState } from "react";
+
+import useStorage from "./hooks/useStorage";
 
 function App() {
-  let [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem("todos"));
-    if (items) {
-      setTodos(items);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const deleteTodo = (delete_id) => {
-    todos = setTodos(todos.filter((todo) => todo.id !== delete_id));
-  };
+  const [todos, setTodos, deleteTodo] = useStorage();
 
   return (
     <div className="App sm:flex block">
